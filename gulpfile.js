d0d9b3f1b316b5bc06dcf8708f01d4c8221a9f0d
Conar Welsh <conar@sellwell.io>
2015-04-22 14:58:43 -0600

    minifications

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,11 @@ elixir(function(mix) {
     mix.browserify('site.js', './public/js/site.js', 'resources/assets/js');
 
     // mix.copy('resources/assets/bower_components/jquery/dist/jquery.js', 'public/vendor/jquery.js');
-    mix.copy('resources/assets/bower_components/jqlite/jqlite.1.1.1.js', 'public/vendor/jquery.js');
-    mix.copy('resources/assets/bower_components/angular/angular.js', 'public/vendor/angular.js');
+    mix.copy('resources/assets/bower_components/jqlite/jqlite.1.1.1.min.js', 'public/vendor/jquery.js');
+    mix.copy('resources/assets/bower_components/angular/angular.min.js', 'public/vendor/angular.js');
     mix.copy('resources/assets/bower_components/modernizr/modernizr.js', 'public/vendor/modernizr.js');
     mix.copy('resources/assets/bower_components/normalize.css/normalize.css', 'public/vendor/normalize.css');
+
+    //version files
+    mix.version(['css/site.css', 'js/site.js']);
 });

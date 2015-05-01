7add095e07a1f5f25ee2c24004376d6887c7f095
Conar Welsh <conar@sellwell.io>
2015-05-01 16:01:00 -0600

    comment out unused code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,8 +21,8 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        //
-    }
+    // protected function schedule(Schedule $schedule)
+    // {
+    //     //
+    // }
 }
